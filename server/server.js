cb452f1ad673f1e089bc6d19ea35787d67f9e214
Rishab Raj <rr0948555@gmail.com>
2025-08-07 23:07:30 +0530

    vercel deployment

--- server/server.js
+++ server/server.js
@@ -13,12 +13,14 @@ const port = 3000;
 await connectDB()
 
 // Middleware
-app.use(express.json())
-app.use(cors())
-app.use(clerkMiddleware())
+app.use(express.json());
+app.use(cors());
+app.use(clerkMiddleware());
 
 // API Routes
 app.get('/', (req, res)=> res.send('Server is Live!'))
 app.use('/api/inngest', serve({ client: inngest, functions }))
 
 app.listen(port, ()=> console.log(`Server listening at http://localhost:${port}`));
+
+export default app;
